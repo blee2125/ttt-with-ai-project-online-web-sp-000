c942fa5a985cdc6a405738073cf26ea37c69e3fd
favorable-device-5158 <blee2125@gmail.com>
2020-01-15 15:46:58 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,10 +6,12 @@ module Players
       if !board.taken?(5)
         move= "5"
       elsif board.turn_count == 1
-
+        move= "1"
       elsif board.turn_count == 2
-
+        move= [1, 3, 7, 9].detect{|m| !board.taken?(m)}.to_s
       elsif board.turn_count == 3
+
+      else
 
       end
     end
