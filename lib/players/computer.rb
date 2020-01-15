7d3355bce1792931154db865949b6092ac17fa83
favorable-device-5158 <blee2125@gmail.com>
2020-01-15 15:49:44 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -9,8 +9,8 @@ module Players
         move= "1"
       elsif board.turn_count == 2
         move= [1, 3, 7, 9].detect{|m| !board.taken?(m)}.to_s
-      elsif board.turn_count == 3
-
+      elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
+        move= "2"
       else
 
       end
