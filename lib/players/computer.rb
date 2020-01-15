c5dbb8ffdf08d3729d267b366cba2c231032e3c5
favorable-device-5158 <blee2125@gmail.com>
2020-01-15 15:33:44 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,16 @@ module Players
   class Computer < Player
 
     def move(board)
+      move= nil
+      if !board.taken?(5)
+        move= "5"
+      elsif board.turn_count == 1
 
+      elsif board.turn_count == 2
+
+      elsif board.turn_count == 3
+
+      end
     end
 
 
